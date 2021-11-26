ab754e469ac29b57a2c79853a990ca7417c95c04
Jaron Rosenau <jaron.rosenau@gmail.com>
2021-11-26 14:53:58 -0800

    change name

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import webpack from 'webpack'
 
 export default {
   head: {
-    titleTemplate: '%s - htmlcoin.info',
+    titleTemplate: '%s - Htmlcoin Block Explorer',
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no'}
